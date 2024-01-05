ef059191ea949008ee93c2efb2e4ccfdd11f56f8
nickonyino <bahatiluiz@gmail.com>
2024-01-05 00:27:33 -0800

    rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+class Rectangle
+def __init__(self, width=0, height=0):
+    self.width = width
+    self.height = height
+
+    @property 
+    def width(self):
+        return self.__width
+
+    @width setter
+    def width (self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an interger")
+        if value < 0:
+            raise ValueError("width must >= 0")
+        self.__width = value
+
+        @prperty 
+        def height(self):
+            return self.__height
+        if not isinstance(value, int):
+            raise TypeError("height must be an interger")
+        if value  < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
