d720d9e8907548a31b302bf72c80bfcdc956579e
nickonyino <nickonyino4@gmailcom>
2024-03-11 07:25:17 -0700

    java

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+/*
+ * create a constant variable called Myvar 
+ * with the value "JavaScript is amazing"
+ */
+
+const myVar = "JavaScript is amazing"
+console.log(myVar);
---
+++ 0x12-javascript-warm_up/README.md
@@ -0,0 +1,4 @@
+JavaScript
+Creatin HTML and setting CSS Style
+Collecting and manipulating video stream from webcam
+Generating 3D graphics and audio sample
