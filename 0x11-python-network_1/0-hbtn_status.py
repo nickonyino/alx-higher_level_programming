189017beb24cc3b75895e4fc309269406ac4f009
nickonyino <bahatiluiz@gmail.com>
2024-03-28 11:16:39 -0700

    network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-import urllib.request
+""" Python script that fetches https://alx-intranet.hbtn.io/status"""
+from urllib.request import urlopen
+
+with urlopen("https://alx-intranet.hbtn.io/status") as response:
+    body = response.read()
+    decoded_content = body.decode('utf-8')
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(decoded_content))
 
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
-        print("Body response:")
-        print("\t- type:", type(html))
-        print("\t- content:", html)
-        print("\t- utf8 content:", html.decode('utf-8'))
