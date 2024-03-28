35309895022c4aa853e19c95bb668a623a7585ae
nickonyino <bahatiluiz@gmail.com>
2024-03-28 13:06:25 -0700

    network

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,21 +1,20 @@
-#!/usr/bin/env python3
-import requests
+#!/usr/bin/python3
+"""
+A Python script that takes 2 arguments in to solve this challenge.
+First argument will be the repository name
+second argument will be the ow
+Don’t need arguement as(number or type)
+"""
 import sys
-
-def get_recent_commits(repo_name, owner_name):
-    url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        commits = response.json()
-        for commit in commits[:10]:
-            sha = commit['sha']
-            author_name = commit['commit']['author']['name']
-            print(f"{sha}: {author_name}")
-    else:
-        print(f"Failed to retrieve commits. Status code: {response.status_code}")
+import requests
 
 if __name__ == "__main__":
     repo_name = sys.argv[1]
     owner_name = sys.argv[2]
-    get_recent_commits(repo_name, owner_name)
+    url = "https://api.github.com/repos/{}/{}/commits"\
+        .format(owner_name, repo_name)
+    response = requests.get(url)
+    commits_list = response.json()
+    for i in range(10):
+        print("{}: {}".format(commits_list[i].get('sha'), commits_list[i].
+              get('commit').get('author').get('name'))))
