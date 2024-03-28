0a058f9d5ffb37e2108490eb473bcee2b39bdd13
nickonyino <bahatiluiz@gmail.com>
2024-03-28 12:37:16 -0700

    network

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Script taking in URL and an email, sends a POST request to
-the passed URL with email as a parameter, and displaying the
+the passed URL with email as a parameter, and displaying
 body of response (decoded in utf-8)
 """
 import sys
@@ -16,6 +16,7 @@ if __name__ == "__main__":
     with urllib.request.urlopen(url, data=data) as response:
         body = response.read().decode('utf-8')
         extracted_email = None
+
         parsed_response = urllib.parse.parse_qs(body)
         if 'email' in parsed_response:
             extracted_email = parsed_response['email'][0]
