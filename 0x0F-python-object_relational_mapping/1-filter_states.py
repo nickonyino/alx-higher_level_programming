1ebe1d3af65e77d32a8b16a87345f956636abed2
nickonyino <nickonyino4@gmailcom>
2024-03-30 12:14:09 -0700

    sql

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,41 +1,17 @@
 #!/usr/bin/python3
-
-import mySQLdb
+"""  lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
 import sys
 
-def lis_state(username, password, database):
-    try:
-        connection = MySQLdb.connect(
-                host =  "localhost",
-                port = 3306,
-                user = username,
-                passwd = password,
-                db = database
-                )
-        cursor =  connection.cursor()
-        querry = "SELECT * FROM states ORDER BY id ASC"
-        cursor.execute(query)
-        states - cursor.fetchall()
 
-        for states in states:
-        print(state)
-    except MySQLdb.Error as e:
-        print("MySQL Eroor:", e)
-
-    finally:
-        if 'çonnection' in locals() and connection.open:
-            cursor.close()
-            connection.close()
-            print("Database connection closed.")
-
-            if __name__ == "__main__":
-                if len(sys.argv) != 4:
-                    print("Usage: python script.py <username> <password> <database>")
-            else:
-                username = sys.argv[1]
-                password = sys.argv[2]
-                database = sys.argv[3]
-                list_states(username, password, database)
-
-
-
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY states.id""")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
