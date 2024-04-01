0f0b3bfe8a876f06ad0265474dfa4efce84c3250
nickonyino <bahatiluiz@gmail.com>
2024-04-01 00:42:01 -0700

    j

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,19 +6,21 @@ than or equal to 400, it prints: Error code: followed by the value of
 the HTTP status code
 """
 import requests
-import sys
+from sys import argv
+
+
+def main(argv):
+    """
+    Method that manage urllib.error.HTTPError exceptions and
+    print: Error code: followed by the HTTP status code
+    """
+    url = argv[1]
+    r = requests.get(url)
+    if r.status_code == requests.codes.ok:
+        print(r.text)
+    else:
+        print("Error code: {}".format(r.status_code))
+
 
 if __name__ == "__main__":
-    # Get URL from command line argument
-    url = sys.argv[1]
-
-    # Send GET request to the URL
-    response = requests.get(url)
-
-    # Check if the status code is greater than or equal to 400
-    if response.status_code >= 400:
-        print("Error code:", response.status_code)
-    else:
-        # Print the body of the response
-        print(response.text)
-
+    main(argv)
