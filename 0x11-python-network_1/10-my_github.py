af26e2feee2ec201a955e2980a187eecf835f00b
nickonyino <bahatiluiz@gmail.com>
2024-03-31 23:49:49 -0700

    j

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""
-Python script that retrieves the 10 most recent commits of a GitHub repository
+"""Uses the GitHub API to display a GitHub ID based on given credentials.
+Usage: ./10-my_github.py <GitHub username> <GitHub password>
+  - Uses Basic Authentication to access the ID.
 """
 import sys
 import requests
@@ -11,4 +12,3 @@ if __name__ == "__main__":
     auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
     r = requests.get("https://api.github.com/user", auth=auth)
     print(r.json().get("id"))
-
