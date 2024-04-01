69432ee1e39a6f3c3946a13481a99817cf14ab65
nickonyino <bahatiluiz@gmail.com>
2024-04-01 00:46:26 -0700

    j

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,41 +1,43 @@
 #!/usr/bin/python3
 """
-Module to access the GitHub API and list recent commits
+The module to the access to the GitHub API and uses its information
 """
 import requests
+from requests.auth import HTTPBasicAuth
 from sys import argv
 
-def print_commits(commit_list):
-    """
-    Print the commit SHA and author name for each commit in the list
-    """
-    for commit in commit_list:
-        sha = commit.get('sha')
-        author = commit.get('commit').get('author').get('name')
-        print(f"{sha}: {author}")
 
 def main(argv):
     """
-    Main function to list recent commits of a repository
+    Function that list 10 commits (from the most recent to oldest)
+    of the repository.The first argument will be the repository name
+    and the second argument is the owner name
     """
-    if len(argv) != 3:
-        print("Usage: ./script.py [repository] [owner]")
-        return
+
+    def print_commits(i, commit_list):
+        """
+        List the commits, less than 10 commits
+        """
+        sha = commit_list[i].get('sha')
+        commit = commit_list[i].get('commit')
+        author = commit.get('author')
+        name = author.get('name')
+        print('{}: {}'.format(sha, name))
 
     repo = argv[1]
     owner = argv[2]
     headers = {"Accept": "application/vnd.github.v3+json"}
-
-    # Send request to GitHub API to fetch commits
-    url = f"https://api.github.com/repos/{owner}/{repo}/commits"
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        commit_list = response.json()
-        print_commits(commit_list[:10])  # Print the 10 most recent commits
+    response = requests.get('https://api.github.com/repos/' + owner +
+                            '/' + repo + '/commits', headers=headers)
+    commit_list = response.json()
+    size = len(commit_list)
+    if size < 10:
+        for i in range(0, size):
+            print_commits(i, commit_list)
     else:
-        print("Failed to fetch commits:", response.status_code)
+        for i in range(0, 10):
+            print_commits(i, commit_list)
+
 
 if __name__ == "__main__":
     main(argv)
-
