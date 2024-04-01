5060fb5233f9a0ad94c0055c074f587634206bd7
nickonyino <bahatiluiz@gmail.com>
2024-03-31 23:47:49 -0700

    j

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,23 +2,13 @@
 """
 Python script that retrieves the 10 most recent commits of a GitHub repository
 """
-
-import requests
 import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
 
 if __name__ == "__main__":
-    repo_name = sys.argv[1]
-    owner_name = sys.argv[2]
-
-    url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        commits = response.json()[:10]  # Get the 10 most recent commits
-        for commit in commits:
-            sha = commit['sha']
-            author_name = commit['commit']['author']['name']
-            print(f"{sha}: {author_name}")
-    else:
-        print("Error fetching commits:", response.status_code)
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
 
