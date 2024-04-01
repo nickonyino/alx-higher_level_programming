612631599d29d48f32b61daf3dfdc38e2a21071e
nickonyino <bahatiluiz@gmail.com>
2024-04-01 13:17:06 -0700

    network

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sending a request to a URL passed as an argument, and displays only the status code
+# Sending a request to the URL passed as an argument, and displays only the status code
 curl -s -o /dev/null -w "%{http_code}" "$1"
