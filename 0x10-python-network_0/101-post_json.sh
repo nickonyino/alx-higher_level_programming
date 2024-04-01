edc0d511cb84d4718cdd354c87e9babf1cd5b3a7
nickonyino <bahatiluiz@gmail.com>
2024-04-01 12:59:58 -0700

    network

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-# Sending JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl -s -H "Content-Type: application/json" -d @"$2" "$1"
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <URL> <filename>"
+    exit 1
+fi
+
+url=$1
+filename=$2
+
+# Check if the file exists
+if [ ! -f "$filename" ]; then
+    echo "Error: File '$filename' does not exist."
+    exit 1
+fi
+
+# Send a POST request with the file content as the body
+curl -sX POST -H "Content-Type: application/json" --data-binary "@$filename" "$url"
+
