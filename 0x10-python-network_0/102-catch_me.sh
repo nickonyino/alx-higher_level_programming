f77b80be4d6de169a2cfc11be1beb0cdf417c5b6
nickonyino <bahatiluiz@gmail.com>
2024-04-01 13:10:01 -0700

    network

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-
-# Send a POST request with custom header
-curl -s -X PUT "0.0.0.0:5000/catch_me" -d "user_id=98" -H "Origin: HolbertonSchool"
-
+# Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
