bb9302f0ed59c89656af23a1fc991ce39d2a0834
nickonyino <bahatiluiz@gmail.com>
2024-04-01 00:36:04 -0700

    j

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """
-The code that takes in a URL, sends a request to the URL and displaying the value of the
-X-Request-Id variable found in  header of the response
+Python script that takes in a URL, sends a request to the URL and
+displays the value of the variable X-Request-Id in the response header
 """
-
-import sys
 import requests
-
+import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
+    response = requests.get(url)
+    x_request_id = response.headers.get('X-Request-Id')
+    if x_request_id:
+        print("X-Request-Id:", x_request_id)
+    else:
+        print("X-Request-Id header not found in the response.")
 
-    r = requests.get(url)
-    print(r.headers.get("X-Request-Id"))
